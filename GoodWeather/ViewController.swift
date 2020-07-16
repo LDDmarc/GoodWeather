a55e635918437780c4d1f174c3b8a4435f1fac86
Daria Leonova <daria.denisovna9713@gmail.com>
2020-07-16 15:48:54 +0300

    Connect to remote

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Дарья Леонова on 16.07.2020.
+//  Copyright © 2020 Дарья Леонова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak private var mainView: UIView!
+    @IBOutlet weak private var textLabel1: UILabel!
+    @IBOutlet weak private var textLabel2: UILabel!
+    
+    var scaleX: CGFloat = -1
+    
+    let tapGestureRecognizer = UITapGestureRecognizer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainView.layer.cornerRadius = 20
+        mainView.layer.cornerCurve = .continuous
+        mainView.addSubview(textLabel1)
+        mainView.addGestureRecognizer(tapGestureRecognizer)
+    
+        tapGestureRecognizer.addTarget(self, action: #selector(rotateView))
+    }
+
+    @objc func rotateView() {
+        UIView.transition(with: mainView, duration: 0.7, options: .transitionFlipFromRight, animations: {
+            self.textLabel1.text = (self.scaleX != -1) ? "Туда" : "Сюда"
+            self.mainView.backgroundColor = (self.scaleX == -1) ? .purple : .systemIndigo
+
+        }, completion: nil)
+        scaleX = (scaleX == -1) ? 1 : -1
+    }
+}
+
